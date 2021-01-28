9ad82bf8095bd6d06185b3974a558a7c94b3adc0
Rufus <apdubrov@gmail.com>
2021-01-29 00:10:02 +0300

    ДЗ 5. Задания 1-3

---
+++ lesson5/task3.py
@@ -0,0 +1,20 @@
+'''
+3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников
+и величину их окладов. Определить, кто из сотрудников имеет оклад менее 20 тыс.,
+вывести фамилии этих сотрудников. Выполнить подсчет средней величины дохода сотрудников.
+'''
+ave_salary = 0
+low_cost_employees = []
+with open("task3.txt", "w", encoding="utf-8") as file:
+    number_of_employees = int(input("Введите количество  сотрудников: "))
+    for i in range(number_of_employees):
+        employee = input("Введите фамилию сотрудника: ")
+        salary = float(input("Введите зарплату сотрудника: "))
+        file.write(f"{employee} {salary} \n")
+        if salary < 20000:
+            low_cost_employees.append(employee)
+        ave_salary += salary
+    ave_salary /= number_of_employees
+
+print(f"Сотрудники с низкой зарплатой:  {low_cost_employees} "
+      f"Средняя зарплата сотрудников: {ave_salary}")
