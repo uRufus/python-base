40443226a64b4db697004d4ae0f18fc43a49dbad
Rufus <apdubrov@gmail.com>
2021-02-03 20:55:39 +0300

    ДЗ 6. Выполнено задание 1

--- lesson6/task1.py
+++ lesson6/task1.py
@@ -9,3 +9,23 @@ color (цвет) и метод running (запуск). Атрибут реали
 Задачу можно усложнить, реализовав проверку порядка режимов,
 и при его нарушении выводить соответствующее сообщение и завершать скрипт.
 '''
+from time import sleep
+
+
+class TrafficLight:
+    __current_color = "красный"
+    traffic_lights = {"красный": [3, "желтый"], "желтый": [2, "зеленый"], "зеленый": [5, "красный"]}
+
+    def running(self, color):
+        if color != self.__current_color:
+            return print("Вы ввели не тот знак светофора")
+        sleep(self.traffic_lights[color][0])
+        self.__current_color = self.traffic_lights[color][1]
+        print(f"Цвет светофора сменился на {self.__current_color}")
+
+
+traffic_light = TrafficLight()
+traffic_light.running("красный")
+traffic_light.running("желтый")
+traffic_light.running("желтый")
+traffic_light.running("зеленый")
